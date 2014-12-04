7bb2bdbaccf474a013ed73769d101ee6ad4fbb39
johnmetzbower <johnmetzbower@nokware.net>
2014-12-03 21:21:56 -0600

    Activity6_1

---
+++ Activity6_1/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Activity6_1
+{
+    class Employee
+    {
+        private int _empID;
+        private string _loginName;
+        //private string _password;
+        private int _securityLevel;
+        
+        public int EmployeeID
+        {
+            get { return _empID; }
+        }
+        public string LoginName
+        {
+            get { return _loginName; }
+            set { _loginName = value; }
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public int SecurityLevel
+        {
+            get { return _securityLevel; }
+        }
+
+        public void Login(string loginName, string password)
+        {
+            LoginName = loginName;
+            Password = password;
+            //Data normally retrieved from database.
+            //Hard coded for demo only.
+            if (loginName == "Smith" & password == "js")
+            {
+                _empID = 1;
+                _securityLevel = 2;
+            }
+            else if (loginName == "Jones" & password == "mj")
+            {
+                _empID = 2;
+                _securityLevel = 4;
+            }
+            else
+            {
+                throw new Exception("login incorrect.");
+            }
+        }
+
+    }
+}
---
+++ Activity6_1/frmLogin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Activity6_1
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //TODO: Your code goes here.
+                Employee oEmployee = new Employee();
+                oEmployee.Login(txtName.Text, txtPassword.Text);
+                MessageBox.Show("Your security level is " + oEmployee.SecurityLevel);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
